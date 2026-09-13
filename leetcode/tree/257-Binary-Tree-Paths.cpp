af95e0a013c8bf3c7968b20a8ccc39ad499a86ef
KAJAL PATEL <kajalpatelofficial2529@gmail.com>
2026-09-13 10:57:59 +0530

    Add Binary-Tree-Paths (tree)

---
+++ leetcode/tree/257-Binary-Tree-Paths.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   void preorder(TreeNode* root, string s, vector<string>& result) {
+     if(root==NULL)return;
+       s+=to_string(root->val);
+       if (root->left == NULL && root->right == NULL) {
+            result.push_back(s);}
+      else{
+        s+="->";
+       
+        preorder(root->left,s,result);
+        preorder(root->right,s,result);
+      }
+    }
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+       vector<string> result;
+       preorder(root, "", result); 
+        
+        return result;
+    }
+};
