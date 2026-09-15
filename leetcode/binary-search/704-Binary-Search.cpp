9336de24b6df4bb932d57c6be5738fb8cd7fbdf7
KAJAL PATEL <kajalpatelofficial2529@gmail.com>
2026-09-15 13:53:25 +0530

    Add Binary-Search (binary-search)

---
+++ leetcode/binary-search/704-Binary-Search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
+        int mid;
+        int found=-1;
+        while(start<=end){
+            mid=start+(end-start)/2;
+          if(nums[mid]==target){
+            found=mid;
+            break;
+          }
+          else if(nums[mid]<target){
+            start=mid+1;
+          }
+          else {
+            end=mid-1;
+          }
+        }
+        return found;
+    }
+};
