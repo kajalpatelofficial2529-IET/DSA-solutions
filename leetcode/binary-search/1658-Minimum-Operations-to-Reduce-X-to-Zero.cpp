541ecafa97641cbbde3db09973ed704ef895582c
KAJAL PATEL <kajalpatelofficial2529@gmail.com>
2026-09-23 10:06:16 +0530

    Add Minimum-Operations-to-Reduce-X-to-Zero (binary-search)

--- leetcode/binary-search/1658-Minimum-Operations-to-Reduce-X-to-Zero.cpp
+++ leetcode/binary-search/1658-Minimum-Operations-to-Reduce-X-to-Zero.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-       int n=nums.size();
-       vector<int>pref(n);
-        pref[0]=nums[0];
-       for(int i=1;i<n;i++){
-          pref[i]=pref[i-1]+nums[i];
-       }
-       int ans=-1;
-        int t=pref[n-1]-x;
-        if(t<0)return -1;
-        int l=0;
-        int sum=0;
-       for(int i=0;i<n;i++){
-        sum+=nums[i];
-       while (sum>t && l<=i){
-            sum-=nums[l];
-            l++;
+        long long sum=0;
+        for(long long s:nums){
+            sum+=s;
         }
-        if (sum==t) {
-                ans=max(ans, i - l + 1);
-            }
-       }
-       if (ans == -1) {
-        return -1;
-       } else {
-       return n - ans;
-       }
+         int n=nums.size();
+       long long target=sum-x;
+       if(target==0)return n;
+
+       int l=0;
+       int ans=-1;
+       long long s=0;
+     for( int r=0;r<n;r++){
+       s+=nums[r];
+      while(s>target && l<=r){
+        s-=nums[l];
+        l++;
+      }
+      if(s==target){
+        ans=max(ans,r-l+1);
+      }
+     }
+   if(ans==-1)return -1;
+    
+   else return n-ans;
     }
 };
